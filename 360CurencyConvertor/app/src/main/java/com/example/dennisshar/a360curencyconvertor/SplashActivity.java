de9ffde11e31b344e7fdbef18a845a162b17a436
Shar, Dennis <dennis.shar@gmail.com>
2018-10-16 18:57:02 +0300

    nerw app added

---
+++ 360CurencyConvertor/app/src/main/java/com/example/dennisshar/a360curencyconvertor/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.dennisshar.a360curencyconvertor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
