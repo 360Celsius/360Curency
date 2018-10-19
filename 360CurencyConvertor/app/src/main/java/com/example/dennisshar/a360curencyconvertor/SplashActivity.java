f12f85f49f13d626e70e37c4720f4a1bd843135a
Shar, Dennis <dennis.shar@gmail.com>
2018-10-19 16:34:46 +0300

    AVLoadingIndicatorView added

--- 360CurencyConvertor/app/src/main/java/com/example/dennisshar/a360curencyconvertor/SplashActivity.java
+++ 360CurencyConvertor/app/src/main/java/com/example/dennisshar/a360curencyconvertor/SplashActivity.java
@@ -3,11 +3,18 @@ package com.example.dennisshar.a360curencyconvertor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wang.avi.AVLoadingIndicatorView;
+
 public class SplashActivity extends BaseActivity {
+
+    AVLoadingIndicatorView avi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        avi = new AVLoadingIndicatorView(getApplicationContext());
+        avi.show();
     }
 }
